3f894c643cb6f8b6c9f2d491aab6186e1db14339
Stefan Frede <stefan@frede.biz>
2012-05-26 01:25:38 +0200

    Add: Cleaned up code and folders.

--- js/script.js
+++ js/script.js
@@ -8,10 +8,6 @@ $(document).ready(function (){
   if (!Modernizr.backgroundsize){
     $.backstretch("/_/img/bg-body.jpg", {speed: 150});
   }
-  $("p.adr a.plain").fancybox({
-    'hideOnContentClick': true
-  });
-
   $("#mailto").click(function() {
     location.href='mailto:public@frede.biz';
     return false;
