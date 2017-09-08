06615af0ed9a07145e7e0ae8afdfc68ae1d1df57
Stefan Frede <public@frede.biz>
2017-09-09 01:59:28 +0200

    Clean-up index.html

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Author: Stefan Frede */
+
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+/* trigger when page is ready */
+$(document).ready(function (){
+  if (!Modernizr.backgroundsize){
+    $.backstretch("/img/bg-body.jpg", {speed: 150});
+  }
+  $("#mailto").click(function() {
+    location.href='mailto:public@frede.biz';
+    return false;
+  });
+});
