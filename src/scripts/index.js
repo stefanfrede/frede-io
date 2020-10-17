5c46ff61da6fc52febf66825954046fb63f42d1f
Stefan Frede <stefan@frede.dev>
2020-10-17 17:14:46 +0000

    feat: move site to 11ty

---
+++ src/scripts/index.js
@@ -0,0 +1,28 @@
+function showTestimonial() {
+  const t = document.querySelectorAll('.js-reviews > .h-review');
+  const n = t[Math.floor(Math.random() * t.length)];
+
+  n.classList.remove('hidden');
+}
+
+function run() {
+  showTestimonial();
+}
+
+if (document.readyState !== 'loading') {
+  run();
+} else {
+  document.addEventListener('DOMContentLoaded', run);
+}
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    try {
+      navigator.serviceWorker.register('/service-worker.js');
+    } catch (error) {
+      console.error('Service worker registration failed: ', error);
+    }
+  });
+}
